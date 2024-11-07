6408b3e98ce0a302a66a59265b1eff6ad469b295
Bruno Vieira Costa <bruno@abstra.app>
2024-11-07 16:21:48 -0300

    ler templates como utf-8

--- abstra_notas/nfse/sp/sao_paulo/templates/__init__.py
+++ abstra_notas/nfse/sp/sao_paulo/templates/__init__.py
@@ -5,5 +5,5 @@ current_path = Path(__file__).parent
 
 
 def load_template(class_name):
-    with open(current_path / f"{class_name}.xml", "r") as file:
+    with open(current_path / f"{class_name}.xml", "r", encoding="utf-8") as file:
         return Template(file.read())
