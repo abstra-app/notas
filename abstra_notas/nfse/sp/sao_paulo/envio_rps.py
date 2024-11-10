0349a1791070feb8bca6321782e1479f25787ddf
Bruno Vieira Costa <bruno@abstra.app>
2024-11-10 19:25:38 -0300

    Normaliz remetente

--- abstra_notas/nfse/sp/sao_paulo/envio_rps.py
+++ abstra_notas/nfse/sp/sao_paulo/envio_rps.py
@@ -304,6 +304,9 @@ class RPS:
 class Remetente:
     remetente: str
 
+    def __post_init__(self):
+        self.remetente = normalizar_cpf_ou_cnpj(self.remetente)
+
     @property
     def remetente_tipo(self) -> Literal["CPF", "CNPJ"]:
         return cpf_ou_cnpj(self.remetente)
